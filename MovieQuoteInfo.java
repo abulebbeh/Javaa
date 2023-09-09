54fa98f41a1236b6b755ccffb8376fc9279238e6
abulebbeh <abulebbehdoaa@gmail.com>
2023-09-09 16:26:27 -0700

    Programming Exercise 1-7

---
+++ MovieQuoteInfo.java
@@ -0,0 +1,10 @@
+public class MovieQuoteInfo
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Yeah, I bet you're a real good catcher -- of donuts in your mouth.");
+        System.out.println("Said by Richie Goodman");
+        System.out.println("in the movie The Benchwarmers");
+        System.out.println("in 2006");
+    }
+}
