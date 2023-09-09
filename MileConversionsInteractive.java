e5a4330124680d43d89d46b8db2087a2a075a8d6
abulebbeh <abulebbehdoaa@gmail.com>
2023-09-09 16:31:45 -0700

    Programming Exercise 2-5

---
+++ MileConversionsInteractive.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class MileConversionsInteractive {
+    public static void main(String[] args) {
+        final double FEET_IN_MILE = 5280;
+        final double INCHES_IN_MILE = 63360;
+        final double YARDS_IN_MILE = 1760;
+        double miles=0;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter miles: ");
+        miles = sc.nextFloat();
+
+        double miles_to_inch = miles*INCHES_IN_MILE;
+        double miles_to_feet = miles*FEET_IN_MILE;
+        double miles_to_yards = miles*YARDS_IN_MILE;
+
+
+        System.out.println("Miles to Inch: "+miles_to_inch);
+        System.out.println("Miles to feet: "+miles_to_feet);
+        System.out.println("Miles to yards: "+miles_to_yards);
+        System.out.println("Number of miles: "+miles);
+        System.out.println(miles+" miles is " + miles_to_inch+" inches,or "+miles_to_feet+" feet, or "+miles_to_yards+" yards");
+    }
+}
