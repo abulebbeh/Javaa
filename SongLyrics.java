f8f2b2e2bdb8070d3a163b3d93c8b2cb981aad1c
abulebbeh <abulebbehdoaa@gmail.com>
2023-09-09 16:21:33 -0700

    Create SongLyrics.java
    Programming Exercise 1-6

---
+++ SongLyrics.java
@@ -0,0 +1,10 @@
+public class SongLyrics
+{
+    public static void main(String[] args)
+    {
+    System.out.println("Imagine life");
+    System.out.println("Imagine life without me");
+    System.out.println("It's a waste of time");
+    System.out.println("Don't want just anybody");
+    }
+}
