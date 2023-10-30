8363937c78901698e13ba0158b42b7e113325f8e
abulebbeh <abulebbehdoaa@gmail.com>
2023-10-29 20:24:00 -0700

    Programming Exercise 9-2

--- ScentedCandle.java
+++ ScentedCandle.java
@@ -1,16 +1,21 @@
 public class ScentedCandle extends Candle {
 	private String scent;
+
+    public ScentedCandle(){
+        this.scent = scent;
+    }
 	
 	public String getScent() {
-		return this.scent;
+		return scent;
 	}
 	
 	public void setScent(String scent) {
 		this.scent = scent;
 	}
 	
+    @Override
 	public void setHeight(int height) {
 		this.height = height;
-		this.price = 3.00 * height;
+		this.price = 3 * height;
 	}
 }
