2081abe010fe5647348021e2124ea1033f6ab0cd
abulebbeh <abulebbehdoaa@gmail.com>
2023-10-29 20:39:34 -0700

    Debugging Exercise 9-1

---
+++ DebugBook.java
@@ -0,0 +1,14 @@
+public class DebugBook
+{
+    public static void main(String[] args){
+     }
+   protected int pages;
+   public DebugBook(int pgs)
+   {
+      this.pages = pgs;
+   }
+   public int getPages()
+   {
+      return pages;
+   }
+}
